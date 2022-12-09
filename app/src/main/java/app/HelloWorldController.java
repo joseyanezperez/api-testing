e7a0158b2f67f13505990784f427dfdcdba5bfda
joseyanezperez <joyanezperez@gmail.com>
2022-12-09 12:36:46 +0100

    First commit

---
+++ app/src/main/java/app/HelloWorldController.java
@@ -0,0 +1,25 @@
+package app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloWorldController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello worlds!";
+
+	}
+
+	@GetMapping("/helloPojo")
+	public HelloPojo sayHelloPojo() {
+
+		HelloPojo helloPojo = new HelloPojo();
+		helloPojo.setValue1("Hello");
+		helloPojo.setValue2("World!");
+		return helloPojo;
+	}
+}

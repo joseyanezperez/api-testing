be1ee4714748e39ce0df948cf2c192c1eefb6f34
Jose Yañez Perez <81964805+joseyanezperez@users.noreply.github.com>
2024-06-10 12:21:51 +0200

    Update HelloWorldServiceTest.java

--- app/src/test/java/com/fenix/api/test/HelloWorldServiceTest.java
+++ app/src/test/java/com/fenix/api/test/HelloWorldServiceTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.fenix.api.HellWorldService;
+import com.fenix.api.HelloWorldService;
 
 public class HelloWorldServiceTest {
 
